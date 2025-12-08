7017176a3c2ecdd5cc02a239fd2d9ec72f71e5ff
Patrick Jason Torres <123430832+trickyson@users.noreply.github.com>
2025-12-08 12:54:04 +0800

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,12 @@
-# db.py
-import mysql.connector
+import os
+import pymysql
 
 def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="", 
-        database="advanced_db"
+    return pymysql.connect(
+        host=os.getenv("MYSQL_HOST", "localhost"),
+        user=os.getenv("MYSQL_USER", "root"),
+        password=os.getenv("MYSQL_PASSWORD", ""),
+        database=os.getenv("MYSQL_DATABASE", "inventory"),
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        cursorclass=pymysql.cursors.DictCursor
     )
